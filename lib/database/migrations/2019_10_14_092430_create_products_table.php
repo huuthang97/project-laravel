c77b69916b5dc9726cbe735161c30fc2f029a8dc
Huu Thang <huuthangk49hce1@gmail.com>
2019-10-14 16:56:30 +0700

    Create table products on db and create products model

---
+++ lib/database/migrations/2019_10_14_092430_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('prod_name');
+            $table->string('prod_slug');
+            $table->bigInteger('prod_price');
+            $table->string('prod_img');
+            $table->string('prod_warranty');
+            $table->string('prod_accessories');
+            $table->string('prod_condition');
+            $table->string('prod_promotion');
+            $table->tinyInteger('prod_status');
+            $table->text('prod_desc');
+            $table->tinyInteger('prod_featured');
+            $table->bigInteger('cate_id')->unsigned();
+            $table->foreign('cate_id')
+                  ->references('id')->on('categories')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
