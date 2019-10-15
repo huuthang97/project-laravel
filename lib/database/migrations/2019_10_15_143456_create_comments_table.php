52a7400702d9bf0637842f577e19bc76d351f2e6
Huu Thang <huuthangk49hce1@gmail.com>
2019-10-15 21:47:11 +0700

    Create comments table on db

---
+++ lib/database/migrations/2019_10_15_143456_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cmt_email');
+            $table->string('cmt_name');
+            $table->string('cmt_content');
+            $table->bigInteger('prod_id')->unsigned();
+            $table->foreign('prod_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
